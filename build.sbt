78ceaf592508ed1d061abbf637c59329881e545d
Ahmed Soliman <ahmed@farghal.com>
2012-07-19 15:33:23 +0200

    added scala client

--- build.sbt
+++ build.sbt
@@ -15,3 +15,7 @@ CompileThriftScrooge.scroogeBuildOptions := List("--verbose", "--finagle")
 libraryDependencies ++= Seq(
    "com.twitter" %% "scrooge-runtime" % "1.1.3"
  )
+
+//mainClass in (Compile, run) := Some("com.cloud9ers.director.ThriftServer")
+
+//mainClass in (Compile, client) := Some("com.cloud9ers.director.ThriftClient")
---
+++ src/main/scala/client.scala
@@ -0,0 +1,31 @@
+package com.cloud9ers.director
+
+import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.Service
+import java.net.InetSocketAddress
+import org.apache.thrift.protocol.TBinaryProtocol
+import com.twitter.finagle.thrift.{ThriftClientFramedCodec, ThriftClientRequest}
+
+object ThriftClient {
+  def main(args: Array[String]) {
+    // Create a raw Thrift client service. This implements the
+    // ThriftClientRequest => Future[Array[Byte]] interface.
+    println("Creating Client...")
+    val service: Service[ThriftClientRequest, Array[Byte]] = ClientBuilder()
+      .hosts(new InetSocketAddress("localhost", 9999))
+      .codec(ThriftClientFramedCodec())
+      .hostConnectionLimit(1)
+      .build()
+
+    // Wrap the raw Thrift service in a Client decorator. The client
+    // provides a convenient procedural interface for accessing the Thrift
+    // server.
+    val client = new LabsDirector.FinagledClient(service, new TBinaryProtocol.Factory())
+
+    client.freeze("Koko Wawa") onSuccess { response =>
+      println("Received response: " + response)
+    } ensure {
+      service.release()
+    }
+  }
+}
